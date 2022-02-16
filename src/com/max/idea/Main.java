b600ca306352b8990bf97873c0d71009bf8ad3f2
Yuliya1403 <89598416+Yuliya1403@users.noreply.github.com>
2022-02-16 18:36:31 +0300

    Update Main.java

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,28 +5,34 @@ import java.util.Locale;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+          Scanner input = new Scanner(System.in);
+
         System.out.println("Введите длину массива: ");
+
         int size = input.nextInt();
         double array[] = new double[size];
+
         System.out.println("Введите элементы массива: ");
-        for (int i = 0; i < size; i++) {
-            array[i] = input.nextDouble();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
         }
-        for (int i = 0; i < size; i++) {
-            double average = 0;
-            double sum = 0;
-            double result = 0;
+
+        double sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
-            average = sum / array.length;
-            result = array[i] * average;
-            System.out.println("умножение на среднее арифметическое: " + result);
         }
+
+        double average = sum / array.length;
+
+        for (int i = 0; i < array.length; i++) {
+            double result = array[i] * average;
+            System.out.println("Умножение на среднее арифметическое: " + result);
+        }
+
     }
 }
-
-
-
 
 
 
